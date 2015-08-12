03c69b474e9088a66ab5118c269ba026dd658cf6
Morgan Lutz and Yelena Belikova <student@epicodus.com>
2015-08-12 10:26:58 -0700

    passed test for case of a tie

--- src/test/java/TestRPSGame.java
+++ src/test/java/TestRPSGame.java
@@ -1,3 +1,5 @@
+import org.junit.*;
+import static org.junit.Assert.*;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -15,9 +17,24 @@ public class TestRPSGame extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  @Test
+  /*@Test
   public void rootTest() {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("Rock Paper Scissors");
   }
+*/
+  @Test
+  public void returnWinner_CheckForCorrectWinner_true(){
+    RPSGame newGame = new RPSGame();
+
+    assertEquals("Congratulations! Paper Wins!", newGame.returnWinner(0, 1));
+
+  }
+
+  @Test
+  public void returnWinner_CheckForTie_true() {
+    RPSGame newGame = new RPSGame();
+
+    assertEquals("Congratulations! You Tied!", newGame.returnWinner(1,1)); 
+    }
 }
