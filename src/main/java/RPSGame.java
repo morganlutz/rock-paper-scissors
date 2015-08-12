58bd838efb1a614ec038c9b2d5ccb98646218743
Morgan Lutz and Yelena Belikova <student@epicodus.com>
2015-08-12 11:22:51 -0700

    added velocity model and random library

--- src/main/java/RPSGame.java
+++ src/main/java/RPSGame.java
@@ -1,7 +1,27 @@
+import java.util.Random;
+import java.util.Arrays;
+import java.util.ArrayList;
 import static java.lang.System.out;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 
 public class RPSGame {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 
@@ -18,8 +38,12 @@ public class RPSGame {
     } else {
       return "Congratulations! You Tied!";
     }
+  }
 
-
+  public static Integer computerChoice () {
+    Random myRandomGenerator = new Random();
+    Integer random = myRandomGenerator.nextInt(3);
+    return random;
   }
 
 }
